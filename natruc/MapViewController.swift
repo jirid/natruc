db99d6c1a854754ff04a908462b417767a47db70
Jiri Dutkevic <jiri@dutkevic.com>
2015-07-12 19:01:45 +0200

    implemented map user interface

--- natruc/MapViewController.swift
+++ natruc/MapViewController.swift
@@ -8,28 +8,109 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UIScrollViewDelegate {
+
+    var chromeVisible = true
+    var initializing = true
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = Natruc.yellow
+
+        //TODO: replace with data from view model
+        let path = NSBundle.mainBundle().pathForResource("map", ofType: "jpg")!
+        let map = UIImage(contentsOfFile: path)!
+
+        imageView.image = map
+        imageView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: map.size.width))
+        imageView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: map.size.height))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if (initializing) {
+
+            scrollView.maximumZoomScale = maxScale()
+            scrollView.minimumZoomScale = minScale()
+            scrollView.zoomScale = midScale()
+
+            initializing = false
+        }
+
+        scrollView.flashScrollIndicators()
+    }
+
+    func minScale() -> CGFloat {
+        return min(scrollView.bounds.width / imageView.image!.size.width, scrollView.bounds.height / imageView.image!.size.height)
+    }
+
+    func midScale() -> CGFloat {
+        return max(scrollView.bounds.width / imageView.image!.size.width, scrollView.bounds.height / imageView.image!.size.height)
+    }
+
+    func maxScale() -> CGFloat {
+        return 0.5
+    }
+
+    @IBAction func tapped(sender: UITapGestureRecognizer) {
+
+        toggleChrome()
     }
     
+    @IBAction func doubleTapped(sender: UITapGestureRecognizer) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        scrollView.setZoomScale(midScale(), animated: true)
+        hideChrome()
     }
-    */
+
+    override func prefersStatusBarHidden() -> Bool {
+
+        return !chromeVisible
+    }
+
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+
+        return imageView
+    }
+
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+
+        if (!initializing) {
+            hideChrome()
+        }
+    }
+
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+
+        if (!initializing) {
+            hideChrome()
+        }
+    }
+
+    func hideChrome() {
+
+        if (chromeVisible) {
+            chromeVisible = false
+            updateChrome()
+        }
+    }
+
+    func toggleChrome() {
+
+        chromeVisible = !chromeVisible
+        updateChrome()
+    }
+
+    func updateChrome() {
+
+        tabBarController?.tabBar.hidden = !chromeVisible
+        setNeedsStatusBarAppearanceUpdate()
+    }
 
 }
